dd8bbf33558e07e2ce12689659fc1d89c7e25d2e
Sirling <mrsirling@gmail.com>
2019-10-23 11:37:58 +0300

    improved search in domains

--- pages/search_page.py
+++ pages/search_page.py
@@ -21,17 +21,12 @@ class SearchPage:
         :param number_of_pages: кількість сторінок для пошуку
         :return: True якщо э співпадіння, в іншому випадку - False
         """
-        # пошук на першій сторінці результатів
-        domains = self.search_results.domains()
-        for domain in domains:
-            if domain_name in domain:
-                return True
         # цикл на пошук
         for page in range(number_of_pages):
-            # перехід на сторінку
-            self.navigation_bar.navigate_to_page(page)
             # пошук усіх доменів з результатів пошуку
             domains = self.search_results.domains()
+            # перехід на сторінку
+            self.navigation_bar.navigate_to_page(page)
             # пошук заданої строки у кожному домені
             for domain in domains:
                 if domain_name in domain:
