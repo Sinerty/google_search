d04e9e888629f3f1387ec2014840457dabe4d5f1
Sirling <mrsirling@gmail.com>
2019-10-23 10:29:48 +0300

    removed en

--- base_elements/base_test.py
+++ base_elements/base_test.py
@@ -5,6 +5,7 @@ class BaseTest:
 
     def setup(self):
         self.driver = Driver().get_instance()   # створення інстансу драйверу
+        self.driver.maximize_window()           # розгортання вікна для firefox
 
     def teardown(self):
         Driver().close()    # закриття вкладки
